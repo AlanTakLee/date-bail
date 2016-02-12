f82e6431281ba58b1879fba61bd5d7b5df7b7c5a
Alan <alan.lee@qooway.com>
2016-02-12 10:20:45 -0800

    initial commit

---
+++ wear/src/main/java/atl/date_bail/MyStubBroadcastActivity.java
@@ -0,0 +1,21 @@
+package atl.date_bail;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("atl.date_bail.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
