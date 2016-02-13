ad4c1672500a47dd97cb5cdef4ec8b4381b48e00
chrisk23 <chris.kuan@hotmail.com>
2016-02-12 17:27:20 -0800

    Date Form Event Detail Edit Activity
    Added text boxes, buttons, spinners for adding event details and custom
    settings (intervals, responses, vibrations)

---
+++ mobile/src/main/java/atl/date_bail/FormActivity.java
@@ -0,0 +1,16 @@
+package atl.date_bail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+
+
+    }
+}
