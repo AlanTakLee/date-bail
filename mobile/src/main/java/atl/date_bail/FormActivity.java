1847bf6be726c71e946f9bfce0a94d1deab30fc2
Alan <alee158@gmail.com>
2016-02-12 20:14:40 -0800

    setup ugly toolbar with callbacks

--- mobile/src/main/java/atl/date_bail/FormActivity.java
+++ mobile/src/main/java/atl/date_bail/FormActivity.java
@@ -2,6 +2,9 @@ package atl.date_bail;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -9,12 +12,37 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
-        //Todo: toolbar
+        setupToolbar();
+
+        //TODO: on create, check is message was passed in
+    }
+
+    private void setupToolbar() {
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.formToolbar);
         toolbar.setTitle("");
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_48dp);
         setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //todo: save form data
-        //TODO: on create, check is message was passed in
+        Button button = (Button) findViewById(R.id.formToolbarSaveBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo: save form data
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
