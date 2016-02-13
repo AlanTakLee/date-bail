0d30ecef7260388ecd11754bc4ae8d916c2da7fe
Alan <alee158@gmail.com>
2016-02-13 05:32:40 -0800

    Remove debug notification call Fixed Id assignment

--- mobile/src/main/java/atl/date_bail/MainActivity.java
+++ mobile/src/main/java/atl/date_bail/MainActivity.java
@@ -57,30 +57,6 @@ public class MainActivity extends AppCompatActivity implements DateFragment.Date
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Log.i("bug", "tapped");
-            BailAlarmer ba = new BailAlarmer();
-            ba.setAlarm(this, 1);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void switchFragment(int position) {
         currentPosition = position;
         Fragment fragment = null;
